7b9b7fef47d0a1ccb7f25eeb82a2fc5c2c74e0f8
lukas123324 <lukas.kuhn@hak-reutte.ac.at>
2024-01-21 11:49:01 +0100

    Fix Class Group + add tests to Group close #4

--- src/formsTesting/Group.java
+++ src/formsTesting/Group.java
@@ -3,9 +3,9 @@ package formsTesting;
 import java.util.ArrayList;
 
 public class Group {
-	
+
 	private ArrayList<Form> forms;
-	
+
 	public Group(){
 		this.forms = new ArrayList<>();
 	}
@@ -17,20 +17,22 @@ public class Group {
 	public void setForms(ArrayList<Form> forms) {
 		this.forms = forms;
 	}
-	
+
+	// Methoden
+
 	/**
 	 * Adds given form to the group.
-	 * 
+	 *
 	 * @param form - geometric form.
 	 */
 	public void addForm(Form form) {
 		this.forms.add(form);
 	}
-	
+
 	/**
 	 * Calculates the sum of the areas
 	 * of all the forms within the group.
-	 * 
+	 *
 	 * @return the sum of the areas
 	 */
 	public double calculateArea() {
@@ -40,21 +42,21 @@ public class Group {
 		}
 		return sum;
 	}
-	
+
 	/**
 	 * Calculates the sum of the circumferences
 	 * of all the forms within the group.
-	 * 
+	 *
 	 * @return the sum of the circumferences
 	 */
 	public double calculateCircumference() {
-		double sum = 1;
+		double sum = 0;
 		for(Form f : this.forms) {
 			sum += f.calculateCircumference();
 		}
 		return sum;
 	}
-	
+
 	/**
 	 * Prints all the forms on the console (text not image).
 	 */
@@ -63,16 +65,30 @@ public class Group {
 			System.out.println(f);
 		}
 	}
-	
+
 	/**
 	 * Returns the biggest form based on area.
-	 * 
+	 *
 	 * @return form with biggest area
 	 */
 	public Form findBiggestForm() {
-		Form form = this.forms.get(0);
-		
-		return form;
+		Form biggestForm = null;
+		double maxArea = 0;
+
+		for(Form f : this.forms) {
+			if (f.calculateArea() > maxArea) {
+				maxArea = f.calculateArea();
+				biggestForm = f;
+			}
+		}
+
+		if(biggestForm == null) {
+			System.out.println("No Forms!");
+		} else {
+			System.out.println("BiggestForm: " + biggestForm);
+		}
+
+		return biggestForm;
 	}
 
 }
--- src/formsTesting/GroupTest.java
+++ src/formsTesting/GroupTest.java
@@ -1,4 +1,50 @@
 package formsTesting;
 
+import java.util.ArrayList;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class GroupTest {
+
+    Circle circle1 = new Circle(1, 1, 3); // 28
+    Circle circle2 = new Circle(1, 1, 4); // 50
+    Rectangle rectangle1 = new Rectangle(1, 1, 5, 4); // 20
+    Rectangle rectangle2 = new Rectangle(1, 1, 5, 6); // 30
+    Square square1 = new Square(1, 1, 7); // 49
+    Square square2 = new Square(1, 1, 5); // 25
+
+    @Test
+    public void calculateArea() {
+        Group group = new Group();
+        group.addForm(circle1); // 28
+        group.addForm(rectangle1); // 20
+        group.addForm(square1); // 49
+        double result = group.calculateArea();
+        Assert.assertEquals(result, (Math.pow(circle1.getRadius(), 2)*Math.PI) + (rectangle1.getLength() * rectangle1.getWidth()) + (square1.getRectangle().getLength() * square1.getRectangle().getWidth()));
+    }
+
+    @Test
+    public void calculateCircumference() {
+        Group group = new Group();
+        group.addForm(circle2);
+        group.addForm(rectangle2);
+        group.addForm(square2);
+        double result = group.calculateCircumference();
+        Assert.assertEquals(result, (2*circle2.getRadius()*Math.PI) + (rectangle2.getLength() * 2 + rectangle2.getWidth() * 2) + (square2.getRectangle().getLength() * 4));
+    }
+
+    @Test
+    public void findBiggestForm() {
+        Group group = new Group();
+        group.addForm(circle1);
+        group.addForm(rectangle1);
+        group.addForm(square1);
+        group.addForm(circle2);
+        group.addForm(rectangle2);
+        group.addForm(square2);
+        Form result = group.findBiggestForm();
+        Assert.assertEquals(result, circle2);
+    }
+
 }
